3b75bcd2f87ed15838b1958d11e3d90d3a880e9e
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-06-25 14:22:40 +0500

    Update NumberDivider.java

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        int toBeDivided = 0;
         double Znachenie = toBeDivided/=5;
         System.out.println(Znachenie);
     }
