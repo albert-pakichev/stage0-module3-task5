ce052bb9b516c9aecd3c725c1ad631608867f321
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-06-25 14:22:59 +0500

    Update TimeConvertor.java

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        int minutes = 0;
         int second = minutes*60;
         System.out.println(second);
     }
